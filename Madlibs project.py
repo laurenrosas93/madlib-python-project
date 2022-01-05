f6d8bef94153230e005d70a29c603524b28f363c
laurenrosas93 <95515803+laurenrosas93@users.noreply.github.com>
2022-01-05 16:53:27 -0500

    Add files via upload

---
+++ Madlibs project.py
@@ -0,0 +1,27 @@
+food_item_title= input("Enter a food item: ")
+number= input("Enter a number:") #variables are declared here
+measurement= input("Enter a measurement: ")
+first_adjective= input ("Enter an adjective: ")
+gerund= input("Enter a verb ending in -ING: ")
+second_adjective= input("Enter another adjective: ")
+adverb_verb=input("Enter an adverb and a verb: ")
+third_adjective= input("Enter a new adjective: ")
+noun= input("Now enter a noun: ")
+fourth_adjective= input("Enter an adjective with a noun: ")
+verb= input("Enter a verb: ")
+second_verb= input ("Enter a second verb: ")
+fifth_adjective= input ("Almost there- enter another adjective: ")
+sixth_adjective= input ("Sorry one more adjective: ")
+second_noun= input("Enter one last noun: ")
+last_adjective= input("Enter your final adjective to read your Mad Lib recipe: ")
+
+
+print("Let's make " + food_item_title + "!") #user enters the food item they will be baking. 
+print("To make " + food_item_title + " you first need to gather your ingredients. You'll need " + number +" eggs, a " + measurement + " of milk, flour, sugar, and " +first_adjective +" butter.")
+print("Start by " +gerund+ " together the " + second_adjective+ " ingredients in a bowl.")
+print("Then, " + adverb_verb + " in the milk, eggs, and " + third_adjective + " butter.")
+print("Once the " + noun + " is " + fifth_adjective + ", you're ready to " + verb+ " !")
+print("Scoop the batter onto a/an"+ fourth_adjective + " and cook until all the bubble "+ second_verb + ".")
+print("The " + sixth_adjective + " part about making " + food_item_title + " is piling them onto your " + second_noun + ", loading them with your " + last_adjective + " toppings, and digging in!")
+
+print("Bon apetit!")
